6ddda54487e6bce4b88674681a90b3f45c45d4cd
Felipe Fernández <38056969+frecall71@users.noreply.github.com>
2023-12-03 19:12:50 -0300

    [ADD] adv

--- helper.js
+++ helper.js
@@ -1,3 +1,17 @@
+/**
+ * Helper functions for creating SVG elements
+ */
+
+
+/**
+ * Creates a rectangle element
+ * @param {Object} group D3 selection of a group element
+ * @param {number} x x coordinate
+ * @param {number} y y coordinate
+ * @param {number} w width
+ * @param {number} h height
+ * @param {string} fill fill color
+ */
 export const createRect = (group, x, y, w, h, fill) => {
     group.append('rect')
       .attr('x', x)
